25a7625f2d1f6474245e20951e8b4ea5e249aaf2
jchile <jchile@unsa.edu.pe>
2023-09-06 05:20:37 -0500

    create imp 3

--- app/src/main/java/com/example/myapplication/container/CreateApiData.kt
+++ app/src/main/java/com/example/myapplication/container/CreateApiData.kt
@@ -2,11 +2,14 @@ package com.example.myapplication.container
 
 import com.example.myapplication.data.ContainerData
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface CreateApiData {
 
+    @GET("all")
+    suspend fun getData(): List<ContainerData>
     @PUT("container")
     suspend fun putData(
         @Body objectCreate: ContainerData
